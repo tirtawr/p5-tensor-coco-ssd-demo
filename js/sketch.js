6b1f814640f579fdd7f50dfd71d82e2db91300d1
Tirta Rachman <tirtawr@10-18-162-174.dynapool.wireless.nyu.edu>
2019-10-11 14:40:05 -0400

    Refactor, clean up the sketch a bit

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,11 @@
-var capture;
+let capture;
+let cocoSsdModel;
+let loadingImage;
+
 var canvas = document.getElementById("sketch-holder");
-var isCocoSsdModelLoaded = false;
-var cocoSsdModel;
 
 function setup() {
-    var p5canvas = createCanvas(960, 960);
+    const p5canvas = createCanvas(680, 480);
     p5canvas.parent('sketch-holder');
     capture = createCapture(VIDEO);
     capture.hide();
@@ -12,43 +13,40 @@ function setup() {
     // Load the model.
     cocoSsd.load().then(_cocoSsdModel => {
         cocoSsdModel = _cocoSsdModel;
-        isCocoSsdModelLoaded = true;
-        console.log('model loaded');
     });
 
-    noLoop();
+    frameRate(10);
 
-    setInterval(function () { draw(); }, 100);
+    loadingImage = loadImage('img/loading.gif');
 }
 
 function draw() {
-    // translate(width, 0);
-    // scale(-1.0, 1.0);
-    image(capture, 0, 0, width, width * capture.height / capture.width);
-
-    if (cocoSsdModel) {
+    if (!cocoSsdModel) {
+        // Show a loading frame when model is still loading
+        textSize(48);
+        textAlign(CENTER, CENTER);
+        text('LOADING', 0, 0, width, height);
+    } else {
+        image(capture, 0, 0, width, width * capture.height / capture.width);
         cocoSsdModel.detect(canvas).then(predictions => {
-            console.log(predictions);
-            for(var i = 0; i < predictions.length; i++) {
-                var prediciton = predictions[i];
-                var bbox = prediciton.bbox;
-                var leftX = bbox[0] / 2
-                var topY = bbox[1] / 2
-                var bottomY = (bbox[1] + bbox[3]) / 2
-                var rightX =  (bbox[0] + bbox[2]) / 2
-                stroke('rgb(0,255,0)');
+            for (let i = 0; i < predictions.length; i++) {
+                let prediciton = predictions[i];
+                let bbox = prediciton.bbox;
+                let leftX = bbox[0] / 2
+                let topY = bbox[1] / 2
+                let bottomY = (bbox[1] + bbox[3]) / 2
+                let rightX = (bbox[0] + bbox[2]) / 2
+                stroke('green');
                 strokeWeight(4);
                 line(leftX, topY, rightX, topY);
                 line(leftX, bottomY, rightX, bottomY);
                 line(leftX, topY, leftX, bottomY);
                 line(rightX, topY, rightX, bottomY);
-                // rect(leftX, topY, 120, 40);
+                textAlign(LEFT, TOP);
                 textSize(18);
                 fill(0);
-                text(prediciton.class, leftX, topY, 120, 40); // Text wraps within text box
+                text(prediciton.class, leftX, topY, 120, 40);
             }
-            console.log('prediction done');
         });
-
     }
 }
